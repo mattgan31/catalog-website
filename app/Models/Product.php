07845def9d293ccd969212d6719d8e0380a74e8d
mattgan31 <alamsyahr00@gmail.com>
2022-12-31 06:36:42 +0700

    Fix upload image product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $table = "products";
+    protected $fillable = ["product_name", "image", "description"];
 }
--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -5,17 +5,30 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md">
+                {{-- <div id="app">
+                    @include('')
+                </div> --}}
                 <div class="card">
                     <div class="card-header">{{ __('Product') }}</div>
 
                     <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
+                        @if (count($products) > 0)
+                            <table>
+                                @foreach ($products as $p)
+                                    <tr>
+                                        <td>
+                                            <img src="{{ asset('images') }}/{{ $p->image }}" alt="{{ $p->product_name }}"
+                                                class="img-thumbnail">
+                                        </td>
+                                        <td>
+                                            <p class="">{{ $p->product_name }}</p>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </table>
+                        @else
+                            <p>The product is not available</p>
                         @endif
-
-                        {{ __('You are logged in!') }}
                     </div>
                     <div class="card-footer">
                         <a href="/admin/product/create" class="btn btn-primary">Add New Product</a>
