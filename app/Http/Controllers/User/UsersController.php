a8a9291d75849459cf794a3dbeaf6a998f901836
Rahmat Alamsyah <57090921+mattgan31@users.noreply.github.com>
2023-03-26 04:34:25 +0700

    Update the userpage

--- app/Http/Controllers/User/UsersController.php
+++ app/Http/Controllers/User/UsersController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UsersController extends Controller
 {
     public function index()
     {
-        return view('user.home');
+        $products = DB::table('products')->limit(3)->get();
+        // error_log($products);
+        return view('user.home', compact('products'));
     }
 }
